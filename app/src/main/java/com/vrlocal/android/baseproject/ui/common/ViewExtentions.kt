8d30969c7b2016da45694f05245f66ce00b9965d
SouravKumarPandit <soupandit@gmail.com>
2020-03-17 17:34:11 +0530

    architecture change

--- app/src/main/java/com/vrlocal/android/baseproject/ui/common/ViewExtentions.kt
+++ app/src/main/java/com/vrlocal/android/baseproject/ui/common/ViewExtentions.kt
@@ -1,15 +1,13 @@
 package com.vrlocal.android.baseproject.ui.common
 
 import android.app.Activity
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import com.vrlocal.uicontrolmodule.common.VUtil
-
-
-
 
 
 fun Activity.makeStatusBarTransparent() {
@@ -46,13 +44,36 @@ fun Activity.makeStatusBarDarkTransparent() {
 }
 //fun  Activity.showToast()
 
-/*
 fun Activity.dp(value: Int): Int {
     return (value / Resources.getSystem().displayMetrics.density).toInt()
 }
+
 fun Activity.px(value: Int): Int {
     return (value * Resources.getSystem().displayMetrics.density).toInt()
-}*/
+}
+
+
+fun Activity.setLightStatusBar(activity: Activity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        var flags =
+            activity.window.decorView.systemUiVisibility // get current flag
+        flags =
+            flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // add LIGHT_STATUS_BAR to flag
+        activity.window.decorView.systemUiVisibility = flags
+        activity.window.statusBarColor = Color.GRAY // optional
+    }
+}
+
+fun Activity.clearLightStatusBar(activity: Activity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        var flags =
+            activity.window.decorView.systemUiVisibility // get current flag
+        flags =
+            flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // use XOR here for remove LIGHT_STATUS_BAR from flags
+        activity.window.decorView.systemUiVisibility = flags
+        activity.window.statusBarColor = Color.GREEN // optional
+    }
+}
 
 //------------------------------------------view  -------------------------------------------------------------------
 
@@ -61,7 +82,8 @@ fun View.setMarginTop(marginTop: Int) {
     menuLayoutParams.setMargins(0, marginTop, 0, 0)
     this.layoutParams = menuLayoutParams
 }
-fun View.setClickListenerBackground(){
+
+fun View.setClickListenerBackground() {
     this.background = VUtil.getDrawableListState(
         VUtil.getRoundDrawable(Color.TRANSPARENT, 8f),
         VUtil.getRoundDrawable(0x9000000, 8f)
